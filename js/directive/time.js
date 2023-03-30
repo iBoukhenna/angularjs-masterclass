f358f5a0a8046c4ca0079cc338db2239da358a71
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-03-30 17:37:20 -0400

    $interval

--- js/directive/time.js
+++ js/directive/time.js
@@ -1,10 +1,15 @@
-app.directive('ngTime', function (dateFilter) {
+app.directive('ngTime', function (dateFilter, $interval) {
     return {
         scope : {},
         restrict: 'E',
         template: '{{time}}',
         link: function (scope, element, attrs) {
             scope.time = dateFilter(new Date(), 'hh:mm:ss');
+
+            interval = $interval(function () {
+                scope.time = dateFilter(new Date(), 'hh:mm:ss');
+                console.log('time change')
+            }, 1000)
         }
     }
 })
