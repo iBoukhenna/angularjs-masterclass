fa8e3e4aa75168056c2130b339f8e3bbbaf57850
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-03-30 16:28:19 -0400

    move content to file

--- js/directive/comment.js
+++ js/directive/comment.js
@@ -1,5 +1,5 @@
 app.directive('ngComment', function () {
     return {
-        template: '<p><strong>{{comment.username | uppercase}}</strong><br>{{comment.content}}</p><hr>'
+        templateUrl: 'partials/_comment.html'
     }
 })
