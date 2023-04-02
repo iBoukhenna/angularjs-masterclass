4850f25b2bbc2eb0e2ee5cb8b483d7fb847f49f3
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-04-02 01:16:35 -0400

    overlap directive

--- js/directive/comment.js
+++ js/directive/comment.js
@@ -9,3 +9,45 @@ app.directive('ngComment', function () {
     }
 })
 
+app.directive('ngTabs', function(){
+    return {
+        restrict: 'E',
+        templateUrl: 'partials/_tabs.html',
+        transclude: true,
+        scope: {},
+        controller: function ($scope) {
+            $scope.tabs = [];
+
+            $scope.select = function (tab) {
+                angular.forEach($scope.tabs, function (t) {
+                    t.selected = false;
+                })
+                tab.selected = true;
+            }
+
+            this.add = function (tab) {
+                if($scope.tabs.length == 0){
+                    $scope.select(tab);
+                }
+
+                $scope.tabs.push(tab);
+            }
+        }
+    }
+})
+
+app.directive('ngTab', function(){
+    return {
+        restrict: 'E',
+        templateUrl: 'partials/_tab.html',
+        transclude: true,
+        scope: {
+            title: '@'
+        },
+        require: '^ngTabs',
+        link: function (scope, element, attrs, tabsCtrl) {
+            scope.selected = false;
+            tabsCtrl.add(scope)
+        }
+    }
+})
