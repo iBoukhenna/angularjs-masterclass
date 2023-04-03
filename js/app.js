f11c76da425566aba6c610b6bdd910cfe475c932
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-04-03 19:28:08 -0400

    clean

--- js/app.js
+++ js/app.js
@@ -1,12 +1,1 @@
-var app = angular.module('myApp', ['ngRoute']);
-app.config(function ($routeProvider) {
-    $routeProvider
-    .when('/', {templateUrl: 'partials/home.html', controller: 'PostsCtrl'})
-    .when('/comments/:id', {templateUrl: 'partials/comments.html', controller: 'CommentsCtrl'})
-    .otherwise({redirectTo: '/'})
-});
-app.filter('round', function () {
-    return function(input, accuracy) {
-        return (Math.round(input * Math.pow(10, accuracy)) / Math.pow(10, accuracy)).toString().replace('.',',');
-    }
-})
+var app = angular.module('myApp');
