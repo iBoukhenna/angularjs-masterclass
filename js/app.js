d5927a82db2b0d8a6aa8d14b2bea35c869b14ac6
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-04-09 12:51:08 -0400

    consume rest ws getById

--- js/app.js
+++ js/app.js
@@ -2,4 +2,5 @@ var app = angular.module('myApp', ['ngResource']);
 app.controller('PostsCtrl', function($scope, $resource) {
     var Post = $resource('http://localhost/comments/:id');
     $scope.posts = Post.query();
+    $scope.post = Post.get({id : 1});
 })
