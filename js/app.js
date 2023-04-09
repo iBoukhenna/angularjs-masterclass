fbaa4fe2776c49c69697e24d58f3608fc9bde264
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-04-09 12:51:23 -0400

    consume rest ws save

--- js/app.js
+++ js/app.js
@@ -3,4 +3,9 @@ app.controller('PostsCtrl', function($scope, $resource) {
     var Post = $resource('http://localhost/comments/:id');
     $scope.posts = Post.query();
     $scope.post = Post.get({id : 1});
+
+    var post = new Post;
+    post.name = "seven";
+    post.content = "My seven comment";
+    post.$save();
 })
