c46f61495ac893b78dbc5f600962b7e247c20ae7
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-04-09 19:21:44 -0400

    add put to consume rest ws update

--- js/app.js
+++ js/app.js
@@ -1,8 +1,14 @@
 var app = angular.module('myApp', ['ngResource']);
 app.controller('PostsCtrl', function($scope, $resource) {
-    var Post = $resource('http://localhost/comments/:id');
+    var Post = $resource('http://localhost/comments/:id', null, {
+        'update': {method:'PUT', params: {id: "@id"}}
+    });
     $scope.posts = Post.query();
-    $scope.post = Post.get({id : 1});
+    $scope.post = Post.get({id : 1}, function () {
+        $scope.post.name = "first";
+        $scope.post.content = "My first comment";
+        $scope.post.$update();
+    });
 
     var post = new Post;
     post.name = "seven";
