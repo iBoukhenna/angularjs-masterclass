0b05d08d9aa25548b4c14026569d1edae1de6c91
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-04-09 21:11:10 -0400

    use window.setInverval doesn't work

--- js/app.js
+++ js/app.js
@@ -4,6 +4,10 @@ app.controller('demoCtrl', function($scope) {
 
     $scope.count = 0;
 
+    window.setInterval(function () {
+        $scope.count++;
+    }, 1000)
+
     $scope.increaseCount = function () {
         $scope.count++;
     }
