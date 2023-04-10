d09e094e124c42d2a957e646ee0300bd827c7ec7
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-04-09 21:15:44 -0400

    use window.setInterval with $apply work

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 var app = angular.module('myApp', []);
 
-app.controller('demoCtrl', function($scope, $interval) {
+app.controller('demoCtrl', function($scope) {
 
     $scope.count = 0;
 
-    $interval(function () {
+    window.setInterval(function () {
         $scope.count++;
+        $scope.$apply();
     }, 1000)
 
     $scope.increaseCount = function () {
