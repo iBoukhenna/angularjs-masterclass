45ae8fea735db7134027f3c939d6270a15b9d543
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-04-09 21:35:57 -0400

    use filter

--- js/app.js
+++ js/app.js
@@ -1,17 +1,20 @@
 var app = angular.module('myApp', []);
 
+app.filter('minutes', function () {
+    return function (input) {
+        console.log('minutes')
+        return Math.floor(input / 60) + " minutes";
+    }
+})
+
 app.controller('demoCtrl', function($scope, $interval) {
 
     $scope.count = 0;
     $scope.counts = [0, 0, 0];
 
     $interval(function () {
-        $scope.count++;
+        $scope.count+=50;
     }, 1000)
-
-    $scope.$watch('count',function (value) {
-        console.log('value=', value);
-    })
 
     $scope.increaseCount = function () {
         $scope.count++;
