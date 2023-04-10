20267a6f384190081233517f61b444d8b65d6825
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-04-09 21:25:40 -0400

    first watcher with $watch

--- js/app.js
+++ js/app.js
@@ -1,14 +1,16 @@
 var app = angular.module('myApp', []);
 
-app.controller('demoCtrl', function($scope) {
+app.controller('demoCtrl', function($scope, $interval) {
 
     $scope.count = 0;
 
-    window.setInterval(function () {
-        $scope.$apply(function () {
-            $scope.count++;
-        });
+    $interval(function () {
+        $scope.count++;
     }, 1000)
+
+    $scope.$watch('count',function (value) {
+        console.log('value=', value);
+    })
 
     $scope.increaseCount = function () {
         $scope.count++;
