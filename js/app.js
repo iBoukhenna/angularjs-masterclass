bb96ed3eaa80e2133c7451dab0c133da2737b93b
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-04-09 21:09:25 -0400

    simple count

--- js/app.js
+++ js/app.js
@@ -2,4 +2,9 @@ var app = angular.module('myApp', []);
 
 app.controller('demoCtrl', function($scope) {
 
+    $scope.count = 0;
+
+    $scope.increaseCount = function () {
+        $scope.count++;
+    }
 })
